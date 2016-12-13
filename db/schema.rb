2e6dc847d5dd632cede5ce729617dc91e75309c4
刘晓燕 <liuxiaoyan16@mails.ucas.ac.cn>
2016-12-13 12:31:09 +0000

    bundle updating sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210124026) do
+ActiveRecord::Schema.define(version: 20161213121840) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
